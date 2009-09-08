688d72ce6e1ed380315a5e5169fb149621506457
Luigi Montanez <luigi.montanez@gmail.com>
2009-09-08 11:49:23 -0400

    Update to Rails 2.3.4. Don't use ostruct anymore.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -20,9 +20,7 @@ Rails::Initializer.run do |config|
   config.gem 'httparty', :version => '>= 0.4.4'
   
   # until ruby-datacatalog is stable, we're vendoring it
-  require 'ostruct'
-  require 'datacatalog'
-  
+  require 'datacatalog'  
   
   config.gem "rspec", :lib => false, :version => ">= 1.2.0" 
   config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
