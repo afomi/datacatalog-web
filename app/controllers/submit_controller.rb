c439bb889df4ff7d5f2f69d2cc68cb7f7d3fa2a8
Luigi Montanez <luigi.montanez@gmail.com>
2009-09-25 10:56:01 -0400

    Language tweak when submitting a data source.

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -7,7 +7,7 @@ class SubmitController < ApplicationController
   def submit
     @data_submission = DataSubmission.new(params[:data_submission])
     if @data_submission.save
-      flash[:notice] = "Thanks! Your submission has been received."
+      flash[:notice] = "Thanks! Your submission has been received. It will be reviewed by our curators."
     else
       render :action => 'index' and return
     end
