51c4172cbef698828c20678459d0742d50524ba9
Luigi Montanez <luigi.montanez@gmail.com>
2009-10-14 11:19:28 -0400

    Display list of Data Suggestions based on folder [Story1443598]

--- app/controllers/admin/data_suggestions_controller.rb
+++ app/controllers/admin/data_suggestions_controller.rb
@@ -1,7 +1,19 @@
 class Admin::DataSuggestionsController < AdminController
   
+  before_filter :set_folders
+  
   def index
-
+    if params[:folder].blank?
+      @folder_name = "Inbox"
+    else  
+      @folder_name = params[:folder]
+    end
+    
+    @suggestions = DataSuggestion.find_tagged_with(@folder_name, :on => :folders)
+  end
+  
+  def set_folders
+    @folders = DataSuggestion.folder_counts
   end
   
 end
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,2 @@
+Time::DATE_FORMATS[:month_and_year] = "%B %Y"
+Time::DATE_FORMATS[:mdy_slashed] = "%m/%d/%Y"
