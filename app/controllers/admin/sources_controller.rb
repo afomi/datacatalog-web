ac3fb16aa552c284614e1d96d1144194c6c38c46
Luigi Montanez <luigi.montanez@gmail.com>
2009-10-23 15:48:48 -0400

    Minor fixes on Sources index curation page

--- app/controllers/admin/sources_controller.rb
+++ app/controllers/admin/sources_controller.rb
@@ -1,7 +1,7 @@
 class Admin::SourcesController < AdminController
   
   def index
-    @sources = DataCatalog::Source.all
+    @sources = DataCatalog::Source.all.sort { |a,b| a.title <=> b.title }
   end
   
 end
