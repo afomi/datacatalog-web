a963184d4f43181e63d157c80d8a9af81205e415
Luigi Montanez <luigi.montanez@gmail.com>
2009-11-02 13:13:21 -0500

    CRUD forms for data sources [Story1498785]

--- app/controllers/admin/sources_controller.rb
+++ app/controllers/admin/sources_controller.rb
@@ -4,4 +4,39 @@ class Admin::SourcesController < AdminController
     @sources = DataCatalog::Source.all.sort { |a,b| a.title <=> b.title }
   end
   
+  def edit
+    @source = DataCatalog::Source.first(:slug => params[:id])
+  end
+  
+  def new
+    @source = DataCatalog::Source.new
+  end
+  
+  def create    
+    @source = DataCatalog::Source.new(params[:source])
+
+    begin
+      source = DataCatalog::Source.create(params[:source])
+      flash[:notice] = "Source created!"
+      redirect_to admin_source_path(source.slug)
+    rescue DataCatalog::BadRequest => e
+      flash[:error] = build_error_msg(e)
+      render :new
+    end
+  end
+
+  def update
+    @source = DataCatalog::Source.get(params[:id])
+    @source.update(params[:source])
+    
+    begin
+      source = DataCatalog::Source.update(params[:id], params[:source])
+      flash[:notice] = "Source saved!"
+      redirect_to edit_admin_source_path(params[:source][:slug])
+    rescue DataCatalog::BadRequest => e
+      flash[:error] = build_error_msg(e)
+      render :edit
+    end
+  end
+  
 end
