e270fde6815b96973291813fbbf1858a1d7940ad
Luigi Montanez <luigi.montanez@gmail.com>
2009-11-04 14:44:00 -0500

    Using RDiscount for Markdown support [Story1567591]

--- config/environment.rb
+++ config/environment.rb
@@ -16,9 +16,11 @@ Rails::Initializer.run do |config|
   config.gem "ruby-openid", :lib => "openid", :version => '>= 2.1.7'
   config.gem 'nokogiri', :version => '>= 1.3.2'
   config.gem 'faker', :version => '>= 0.3.1'
+  config.gem 'rdiscount', :version => '>= 1.3.5'
+
   config.gem 'luigi-httparty', :lib => 'httparty', :version => '>= 0.4.4'
-  config.gem 'datacatalog', :version => '>= 0.3.2'
-  
+  config.gem 'datacatalog', :version => '>= 0.3.4'
+
   config.gem "rspec", :lib => false, :version => ">= 1.2.0" 
   config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
   config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :version => ">= 2.10.0", :source => 'http://gems.github.com'
