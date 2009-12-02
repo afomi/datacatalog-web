7e5fa298f4ceb68dced0400f1bb7d85b464ea50c
Luigi Montanez <luigi.montanez@gmail.com>
2009-12-02 11:27:26 -0500

    Finish up Dashboard screen [Story1505596]

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,23 @@ class MainController < ApplicationController
   
   def dashboard
     if current_user
+      @recent_comments = DataCatalog::Comment.all(:user_id => current_user.api_id)
+      @recent_comments.each do |c|
+        source = DataCatalog::Source.get(c.source_id)
+        c.source_title = source.title
+        c.source_slug = source.slug
+      end
+
+      @all_ratings = DataCatalog::Rating.all(:user_id => current_user.api_id)
+      @recent_ratings = []
+      @all_ratings[0..5].each do |rating|
+        if rating.kind == "source"
+          source = DataCatalog::Source.get(rating.source_id)
+          rating.source_title = source.title
+          rating.source_slug = source.slug
+          @recent_ratings << rating
+        end
+      end
       render 'dashboard' and return
     else
       render 'welcome' and return
