36e1c1956a255eba5542ae8ea14afe4f4669f121
Luigi Montanez <luigi.montanez@gmail.com>
2009-12-08 18:05:26 -0500

    Bump up Rails and datacatalog gem.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   config.gem 'rdiscount', :version => '>= 1.3.5'
 
   config.gem 'luigi-httparty', :lib => 'httparty', :version => '>= 0.4.4'
-  config.gem 'datacatalog', :version => '>= 0.3.6'
+  config.gem 'datacatalog', :version => '>= 0.4.2'
 
   config.gem "rspec", :lib => false, :version => ">= 1.2.0" 
   config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
