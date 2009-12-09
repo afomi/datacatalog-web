2dfdf6b6b211e3579e3ad80fbd63fa9c62b49d8d
Luigi Montanez <luigi.montanez@gmail.com>
2009-12-09 18:13:21 -0500

    Pagination for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
   def index
     @term = params[:term]
     @sources = @term.blank? ? [] : DataCatalog::Source.search(@term)
+    @page = params[:page].nil? ? 1 : params[:page].to_i
   end
   
 end
