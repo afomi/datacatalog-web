7faab8cf797e6fffe0dc364650e48ae9ded5dddc
David James <djames@sunlightfoundation.com>
2009-12-10 17:45:22 -0500

    better pagination navigation on /browse

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -3,6 +3,9 @@ class BrowseController < ApplicationController
   def index
     @sources = DataCatalog::Source.all
     @page = params[:page].nil? ? 1 : params[:page].to_i
+    @pages = 1.upto(@sources.page_count).map do |i|
+      { :number => i, :path => "?page=#{i}" }
+    end
   end
   
 end
