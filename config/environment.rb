1c5f9b0f3601f62b97fe6aba9437e2697a843310
David James <djames@sunlightfoundation.com>
2009-12-11 18:37:30 -0500

    add delayed_job (migration required)

--- config/environment.rb
+++ config/environment.rb
@@ -30,6 +30,7 @@ Rails::Initializer.run do |config|
   config.gem 'webrat', :version => '>= 0.4.5'
   config.gem 'cucumber', :version => '>= 0.3.94', :source => 'http://gems.github.com'
   config.gem 'bmabey-database_cleaner', :lib => 'database_cleaner', :version => '>= 0.2.2', :source => 'http://gems.github.com'
+  config.gem 'delayed_job', :lib => 'delayed_job'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
