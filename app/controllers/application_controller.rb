df2708b09ab41a722610f6f92db1e617517e09d6
Luigi Montanez <luigi.montanez@gmail.com>
2010-01-11 14:52:56 -0500

    Tweak language when taking actions that require sign in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -53,7 +53,7 @@ class ApplicationController < ActionController::Base
   def require_user
     unless current_user
       store_location
-      flash[:notice] = "You must be logged in to access this page"
+      flash[:notice] = "You must be logged in to take that action."
       redirect_to signin_url
       return false
     end
@@ -62,7 +62,7 @@ class ApplicationController < ActionController::Base
   def require_no_user
     if current_user
       store_location
-      flash[:notice] = "You must be logged out to access this page"
+      flash[:notice] = "You must be logged out to take that action."
       redirect_to profile_url
       return false
     end
