8e724d7cc54eed303a3dea6a6a88dd14a9fbcc0c
David James <djames@sunlightfoundation.com>
2010-01-28 13:39:52 -0500

    regenerate dj_cache files after delete [#2322447]
    @queued[label] was not a good approach. Removing it.
    
    "Q: What can I do to prevent duplicate jobs from being queued up?
    Shouldn’t there be a check to see if the handler is the same for a
    job?" -http://wiki.github.com/tobi/delayed_job/ (no answer yet)

--- lib/cache.rb
+++ lib/cache.rb
@@ -2,17 +2,15 @@ class Cache
   
   CACHE_PATH = "#{RAILS_ROOT}/tmp/dj_cache/"
 
-  def initialize
-    @queued = {}
-  end
-
   LABELS = %w(
     organizations
     active_organizations
   )
   
   def get(label, cache_invalid_after=3600)
-    run_unless_already_queued(label) if cache_invalid?(label)
+    if cache_invalid?(label)
+      self.send_later(:run_query, label)
+    end
     load_from_file(label)
   end
   
@@ -25,34 +23,25 @@ class Cache
     (Time.now - modified_at) > cache_life
   end
   
-  def run_unless_already_queued(label)
-    if !@queued[label]
-      self.send_later(:run_query, label)
-      @queued[label] = true
-    else
-      puts "#{label} is already in the queue"
-    end
-  end
-  
   def run_query(label)
+    logger = Delayed::Worker.logger
     o1 = case label
     when :organizations
-      puts "Loading all organizations..."
+      logger.info "Loading all organizations..."
       DataCatalog::Organization.all
     when :active_organizations
-      puts "Loading organizations with at least one source..."
+      logger.info "Loading organizations with at least one source..."
       DataCatalog::Organization.all('source_count > 0')
     else
       raise RuntimeError, "label not found : #{label}"
     end
-    puts "Building array with name and id..."
+    # logger.info "Building array with name and id..."
     o2 = o1.map { |o| [o.name, o.id] }
-    puts "Sorting array..."
+    # logger.info "Sorting array..."
     o3 = o2.sort_by { |x| x[0] }
-    puts "Saving..."
+    # logger.info "Saving..."
     write_to_file(label, o3)
-    puts "Done."
-    @queued[label] = false
+    logger.info "Done."
   end
   
   def filename(label)
