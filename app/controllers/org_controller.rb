2dc44c3c53038e248461b313e68599b3ddbf13fc
David James <djames@sunlightfoundation.com>
2010-04-13 17:26:26 -0400

    Show sources for top-level orgs [#3140204]
    Top-level organizations are shown to users as jurisdictions; e.g.
    DC, Utah, New York, etc.

--- app/controllers/org_controller.rb
+++ app/controllers/org_controller.rb
@@ -3,9 +3,19 @@ class OrgController < ApplicationController
   
   def show
     @org = DataCatalog::Organization.first(:slug => params[:slug])
-    @sources = DataCatalog::Source.all(:organization_id => @org.id)
+    @sources = find_sources(@org)
     @pages = paginate(@sources)
     @clear = false
+  end
+  
+  protected
+  
+  def find_sources(org)
+    if @org.top_level
+      DataCatalog::Source.all(:jurisdiction_id => @org.id)
+    else
+      DataCatalog::Source.all(:organization_id => @org.id)
+    end
   end
   
 end
