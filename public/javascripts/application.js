89c54eeff9b042a93a443033dd6ae6ab39e21a6f
Luigi Montanez <luigi.montanez@gmail.com>
2010-04-26 18:38:40 -0400

    Add jQueryUI DatePicker to Released On, Period Start, Period End [#3260423]

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -32,6 +32,10 @@ $(document).ready(function(){
       } 
   );
   
+  $("#released").datepicker({ changeYear: true, changeMonth: true });
+  $("#period_start").datepicker({ changeYear: true, changeMonth: true });
+  $("#period_end").datepicker({ changeYear: true, changeMonth: true });
+  
   $("li#moveBtn a.buttonLink").toggle(
     function(){
        $(this).addClass("active");
