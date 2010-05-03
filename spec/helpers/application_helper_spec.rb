7814cbb02f86123062fba7d81147591c9bae2a21
David James <djames@sunlightfoundation.com>
2010-05-03 14:17:42 -0400

    Correct specs for active_if (use arrays)

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,22 +5,22 @@ describe ApplicationHelper do
   describe "active_if" do
     it "when controller matches" do
       params[:controller] = "search"
-      helper.active_if(:controller => "search").should == "active"
+      helper.active_if(:controller => ["search"]).should == "active"
     end
 
     it "when controller does not match" do
       params[:controller] = "search"
-      helper.active_if(:controller => "not_search").should == ""
+      helper.active_if(:controller => ["not_search"]).should == ""
     end
 
     it "when action matches" do
       params[:action] = "index"
-      helper.active_if(:action => "index").should == "active"
+      helper.active_if(:action => ["index"]).should == "active"
     end
 
     it "when action does not match" do
       params[:action] = "index"
-      helper.active_if(:action => "not_index").should == ""
+      helper.active_if(:action => ["not_index"]).should == ""
     end
   end
 
