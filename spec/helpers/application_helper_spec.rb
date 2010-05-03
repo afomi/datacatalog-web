44161a2c506be78556bc933c70c7cdab156409d3
David James <djames@sunlightfoundation.com>
2010-05-03 13:48:12 -0400

    Add test for active_if helper

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,27 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ApplicationHelper do
+
+  describe "active_if" do
+    it "when controller matches" do
+      params[:controller] = "search"
+      helper.active_if(:controller => "search").should == "active"
+    end
+
+    it "when controller does not match" do
+      params[:controller] = "search"
+      helper.active_if(:controller => "not_search").should == ""
+    end
+
+    it "when action matches" do
+      params[:action] = "index"
+      helper.active_if(:action => "index").should == "active"
+    end
+
+    it "when action does not match" do
+      params[:action] = "index"
+      helper.active_if(:action => "not_index").should == ""
+    end
+  end
+
+end
