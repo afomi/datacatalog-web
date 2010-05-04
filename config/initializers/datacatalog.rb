0723e4970b51b2472a5669e80f7d56adc59aad1a
David James <djames@sunlightfoundation.com>
2010-05-04 12:49:53 -0400

    Complain if api.yml is misconfigured

--- config/initializers/datacatalog.rb
+++ config/initializers/datacatalog.rb
@@ -1,6 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
-dc_config = YAML.load_file(File.dirname(__FILE__) + "/../api.yml")
-
-DataCatalog.api_key   = dc_config[RAILS_ENV]['api_key']
-DataCatalog.base_uri  = dc_config[RAILS_ENV]['base_uri']
+file = File.expand_path(File.dirname(__FILE__) + "/../api.yml")
+api_config = YAML.load_file(file)
+api_config_env = api_config[RAILS_ENV]
+unless api_config_env
+  raise %(Environment "#{RAILS_ENV}" not found in #{file})
+end
+unless api_config_env['api_key']
+  raise %(api_key not found for environment "#{RAILS_ENV}" in #{file})
+end
+unless api_config_env['base_uri']
+  raise %(base_uri not found for environment "#{RAILS_ENV}" in #{file})
+end
+DataCatalog.api_key  = api_config_env['api_key']
+DataCatalog.base_uri = api_config_env['base_uri']
